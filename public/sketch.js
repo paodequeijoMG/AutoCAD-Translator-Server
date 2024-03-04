f5090d7bd6cae608cf9db96130f79c739a076dec
paodequeijoMG <diogo.braz@hotmail.com>
2024-03-04 21:01:18 +0000

    2

--- public/sketch.js
+++ public/sketch.js
@@ -97,7 +97,7 @@ async function getData() {
         body: JSON.stringify(api_array)
     }
     // const url_lh = new URL("https://autocad-translator-server.vercel.app/");
-    const resposta = await fetch("api", options);
+    const resposta = await fetch("/", options);
     const json = await resposta.json();
     console.log(json);
 
