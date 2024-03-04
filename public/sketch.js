5965735801831409c88e7edb98a2ab8a25d55d94
paodequeijoMG <diogo.braz@hotmail.com>
2024-03-04 21:19:18 +0000

    "qweqw"

--- public/sketch.js
+++ public/sketch.js
@@ -87,64 +87,64 @@ console.log(api_array);
 
 // getData();
 
-async function getData() {
+// async function getData() {
 
-    const options = {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(api_array)
-    }
-    // const url_lh = new URL("https://autocad-translator-server.vercel.app/");
-    const resposta = await fetch("/servidor.html", options);
-    const json = await resposta.json();
-    console.log(json);
+//     const options = {
+//         method: 'POST',
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(api_array)
+//     }
+//     // const url_lh = new URL("https://autocad-translator-server.vercel.app/");
+//     const resposta = await fetch("/servidor.html", options);
+//     const json = await resposta.json();
+//     console.log(json);
 
-    const text_translated_array = [];
-    for (let i = 0; i < json.texto.length; i++) {
-        text_translated_array.push(json.texto[i].text)
-    }
+//     const text_translated_array = [];
+//     for (let i = 0; i < json.texto.length; i++) {
+//         text_translated_array.push(json.texto[i].text)
+//     }
 
-    const data_array_translated = data_array.map(innerArray => [...innerArray]);;
-    let index = 0;
-    for (let i = 0; i < data_array.length; i++) {
-        for (let j = 3; j < data_array[i].length; j += 3) {
-            data_array_translated[i][j] = text_translated_array[index];
-            index++;
-        }
-    }
-    const lines = data_array_translated.map(innerArray => innerArray.join(';'));
-    const outputText = decodeHtmlEntities(lines.join(';\n') + ";");
+//     const data_array_translated = data_array.map(innerArray => [...innerArray]);;
+//     let index = 0;
+//     for (let i = 0; i < data_array.length; i++) {
+//         for (let j = 3; j < data_array[i].length; j += 3) {
+//             data_array_translated[i][j] = text_translated_array[index];
+//             index++;
+//         }
+//     }
+//     const lines = data_array_translated.map(innerArray => innerArray.join(';'));
+//     const outputText = decodeHtmlEntities(lines.join(';\n') + ";");
 
-    // Create a Blob with the JSON data
-    const downloadLink = document.getElementById("dl_txt");
-    const text_blob = new Blob([outputText], { type: 'text/plain' });
+//     // Create a Blob with the JSON data
+//     const downloadLink = document.getElementById("dl_txt");
+//     const text_blob = new Blob([outputText], { type: 'text/plain' });
     
-    // Set the download link attributes
-    downloadLink.href = window.URL.createObjectURL(text_blob);
-    downloadLink.download = 'api_response.txt';
-    downloadLink.style.display = 'none';
+//     // Set the download link attributes
+//     downloadLink.href = window.URL.createObjectURL(text_blob);
+//     downloadLink.download = 'api_response.txt';
+//     downloadLink.style.display = 'none';
 
-    // Add an event listener to execute callback function when download is complete
-    downloadLink.addEventListener('click', function() {
-        // Ensure that the download is completed before calling callback
-        if (downloadLink.href.startsWith('blob:')) {
-            // Execute the callback function after a short delay (adjust the delay as needed)
-            setTimeout(function() {
-                // Assuming that Acad is globally accessible (replace with the actual global object)
-                if (Acad && Acad.Editor) {
-                    closeBrowser(downloadLink.href, url);
-                } else {
-                    console.error('Acad or Acad.Editor is not accessible. Unable to execute callback.');
-                }
-            }, 1000);
-        }
-    });
+//     // Add an event listener to execute callback function when download is complete
+//     downloadLink.addEventListener('click', function() {
+//         // Ensure that the download is completed before calling callback
+//         if (downloadLink.href.startsWith('blob:')) {
+//             // Execute the callback function after a short delay (adjust the delay as needed)
+//             setTimeout(function() {
+//                 // Assuming that Acad is globally accessible (replace with the actual global object)
+//                 if (Acad && Acad.Editor) {
+//                     closeBrowser(downloadLink.href, url);
+//                 } else {
+//                     console.error('Acad or Acad.Editor is not accessible. Unable to execute callback.');
+//                 }
+//             }, 1000);
+//         }
+//     });
 
-    // Trigger a click on the link to initiate the download
-    downloadLink.click();
-}
+//     // Trigger a click on the link to initiate the download
+//     downloadLink.click();
+// }
 
 function closeBrowser(reference, _url) {
     window.URL.revokeObjectURL(reference);
